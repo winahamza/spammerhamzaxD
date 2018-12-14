59ecf39d1b0116c01f9c1e42ae10b49342c29f0f
winahamza <45149646+winahamza@users.noreply.github.com>
2018-12-15 00:03:17 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("522486253397082114")
+setInterval(function() {
+channel.send(`i need credit`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
